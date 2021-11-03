f23eaac682410914eb415f14646f5507a1c6d918
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-11-03 23:40:31 +0600

    feat: genserver for random message

---
+++ lib/radom_greeting_server/random_greeting_server.ex
@@ -0,0 +1,38 @@
+defmodule GoodMorningBot.RandomGreetingServer do
+  use GenServer
+
+
+  @impl true
+  def init(_) do
+    {:ok, load_greetings()}
+  end
+
+  @impl true
+  def handle_call(:get_greetings, _from, state) do
+    {:reply, random_greeting(state), state}
+  end
+
+  defp load_greetings do
+    file_path = Path.join(:code.priv_dir(:good_morning_bot), "language_greeting_pair.txt")
+    {:ok, file} = File.read(file_path)
+    String.split(file, "\n")
+  end
+
+  defp clean_up(greeting_dirty) do
+    Regex.run(~r/"(.*)": "(.*)"/, greeting_dirty)
+  end
+
+  defp extract_language_and_greeting(list_of_info) do
+    [_, language, greeting] = list_of_info
+    greeting <> " Language: " <> language
+  end
+
+  defp random_greeting(greetings) do
+
+    greetings
+    |> Enum.random()
+    |> clean_up()
+    |> extract_language_and_greeting()
+
+  end
+end
