cd8e3842da87182feabb47c88d7798b456567bf4
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-11-06 19:46:37 +0600

    fix: corntab time set to 10:00AM
    before this, corn tab minute field wasn't set.
    so it ran 60 times. 🤦‍♂️

--- config/config.exs
+++ config/config.exs
@@ -5,5 +5,5 @@ config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
 config :good_morning_bot, GoodMorningBot.Scheduler,
     timezone: "Asia/Dhaka",
     jobs: [
-      {"* 10 * * *",           {GoodMorningBot, :send_message_to_discord, []}}
+      {"0 10 * * *",           {GoodMorningBot, :send_message_to_discord, []}}
     ]
